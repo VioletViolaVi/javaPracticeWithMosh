d2fd856a465efcbafcac300ec6f5efb8626774db
VioletViola <vivian.v18723@gmail.com>
2021-09-05 20:44:06 +0100

    Use Point class to note difference between reassigning reference and primitive variables.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.util.Date;
 
 public class Main {
@@ -5,10 +6,10 @@ public class Main {
         // Primitive data types
         System.out.println("hi world");
         byte age = 1;
-        System.out.println(age);
+        System.out.println("age: " + age);
         int myAge = 15;
         int herAge = myAge;
-        System.out.println(herAge);
+        System.out.println("herAge: " + herAge);
         /*
             need both "long" and "L" for numbers larger or
             smaller than 2 million or -2 million respectively
@@ -21,9 +22,26 @@ public class Main {
         float price = 10.99F;
         char letter = 'A';
         boolean isEligible = false;
+        char letter1 = 'T';
+        char letter2 = letter1;
+        System.out.println("letter1: " + letter1);
+        System.out.println("letter2: " + letter2);
+        letter1 = 'U';
+        System.out.println("letter2: " + letter2);
+        System.out.println("letter1: " + letter1);
 
         // Reference data types
         Date now = new Date();
-        System.out.println(now);
+        System.out.println("now: " + now);
+
+        Point point1 = new Point(1, 1);
+        Point point2 = point1;
+        System.out.println("point1: " + point1);
+        System.out.println("point2: " + point2);
+
+        point1.x = 5;
+        point1.y = 5;
+        System.out.println("point1: " + point1);
+        System.out.println("point2: " + point2);
     }
 }
