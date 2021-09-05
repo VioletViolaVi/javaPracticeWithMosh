b6b17c4bb17e01082dd39d9c8a7f7d879acd695b
VioletViola <vivian.v18723@gmail.com>
2021-09-05 19:47:04 +0100

    Use long data type with correct syntax.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("hi world");
+        byte age = 1;
+        System.out.println(age);
+        int myAge = 15;
+        int herAge = myAge;
+        System.out.println(herAge);
 
+        /*
+            need both "long" and "L" for numbers larger or
+            smaller than 2 million or -2 million respectively
+        */
+        long youtubeViewsCount = 3_123_456_789L;
     }
 }
